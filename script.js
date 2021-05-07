881743122170e38d6b8f6c427f0d1f88dc9774bf
Andrew Masih <armasih@outlook.com>
2021-05-07 10:18:02 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+/* you can use it by using the 'if' or 'else' or 'switch' statement, conditionals are like direction at a crossroad, you can go left or right, true or false.*/
+
+/* conditional statements control the flow of your programme, it means it controls how your code runsunder certain conditions, some code block block will run only of it is true, if false it will not run.  */
+
+let sky = "blue";
+
+if (sky === "blue") {
+	/* sky is blue, meaning the condition is TRUE.
+	 That is why console.log will run */
+  console.log("Nice weather");
+} else {
+  /* If sky was something other than blue, this code block would run */
+  console.log("Not nice weather");
+}
+
+/* We set the variable sky to blue. If the sky is blue, 
+then first console.log will run. Otherwise second console.log will run.
+Try changing sky to "gray" and see what happens */
